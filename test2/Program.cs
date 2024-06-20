aaf7d249516c3284c4f99aefb8b73ddee3855bf6
denghe <denghe@hotmail.com>
2024-06-20 21:45:23 +0800

    simple test ok

--- test2/Program.cs
+++ test2/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 /*
@@ -10,62 +9,107 @@ https://github.com/forrestthewoods/fts_unity_native_plugin_reloader
 //using UnityEngine;
 // #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
 // #endif
+//public static delegate* unmanaged[Stdcall]<int> Init = (delegate* unmanaged[Stdcall]<int>)GetProcAddress(hDll, "Init");
 
-public unsafe static class Dll /*: MonoBehaviour*/ {
+public static class Dll
+{
+
+    public static System.IntPtr dllHandle;
 
     /*************************************************************************/
     // core funcs
 
     [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
-    public static extern IntPtr LoadLibrary(string lpLibFileName);
+    public static extern System.IntPtr LoadLibrary(string lpLibFileName);
 
     [DllImport("kernel32", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool FreeLibrary(IntPtr hModule);
+    public static extern bool FreeLibrary(System.IntPtr hModule);
 
     [DllImport("kernel32")]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+    public static extern System.IntPtr GetProcAddress(System.IntPtr hModule, string lpProcName);
+
+    [DllImport("kernel32.dll")]
+    static private extern uint GetLastError();
 
     /*************************************************************************/
-    // mapped funcs
 
-    public static IntPtr hDll;
-    public static delegate* unmanaged[Stdcall]<int> Init;
+    public static void LoadDll()
+    {
+        Debug.Assert(dllHandle == System.IntPtr.Zero);
 
-    /*************************************************************************/
-    // utils
-
-    public static void LoadDll() {
-        Debug.Assert(hDll == IntPtr.Zero);
-
-        hDll = LoadLibrary(
-#if DEBUG
-        "../../../../x64/Debug/dll.dll"
+        dllHandle = LoadLibrary(
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        Application.dataPath + "/Plugins/"
 #else
-        "../../../../x64/Release/dll.dll"
+#if DEBUG
+        "../../../../x64/Debug/"
+#else
+        "../../../../x64/Release/"
 #endif
-        );
+#endif
+         + "dll.dll");
 
-        Init = (delegate* unmanaged[Stdcall]<int>)GetProcAddress(hDll, "Init");
+        var t = typeof(Dll);
+        var fields = t.GetFields(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+
+        foreach (var field in fields)
+        {
+            if (field.Name == "dllHandle") continue;
+
+            var fnPtr = GetProcAddress(dllHandle, field.Name);
+            if (fnPtr != System.IntPtr.Zero)
+            {
+                field.SetValue(null, Marshal.GetDelegateForFunctionPointer(fnPtr, field.FieldType));
+            }
+        }
+    }
+
+    public static void UnloadDll()
+    {
+        Debug.Assert(dllHandle != System.IntPtr.Zero);
+        FreeLibrary(dllHandle);
+        dllHandle = System.IntPtr.Zero;
         // ...
     }
 
-    public static void UnloadDll() {
-        Debug.Assert(hDll != IntPtr.Zero);
-        FreeLibrary(hDll);
-        hDll = IntPtr.Zero;
-        Init = null;
-        // ...
-    }
+    /*************************************************************************/
+    // more dll funcs here ( auto map )
+
+    public static Init_ Init;
+    public delegate int Init_();
+
+    // ...
 }
 
 /************************************************************************************************************************/
 /************************************************************************************************************************/
 
-public unsafe static class MainClass {
+/*
+ * put these code into ENTRY class
+
+void Awake()
+{
+    Dll.LoadDll();
+}
+
+void OnDestroy() {
+    Dll.UnloadDll();
+}
+
+*/
+
+
+/************************************************************************************************************************/
+/************************************************************************************************************************/
+
+// invoke tests
+public static class MainClass {
     public static void Main() {
         Dll.LoadDll();
+
         Console.WriteLine(Dll.Init());
+
         Dll.UnloadDll();
     }
 }
